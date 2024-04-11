82378a23b441509e84a48572a55e24ca67d23143
Iban Montero <imontero@student.42urduliz.com>
2024-04-11 18:13:11 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -2,8 +2,6 @@ all:
 	docker-compose -f srcs/docker-compose.yml  up -d --build
 down:
 	docker-compose -f srcs/docker-compose.yml  down
-py:
-	docker run -it srcs-py bash
 clean: down
 	yes | docker system prune -a
 .PHONY: all down clean
--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
 
-# wordpress configuration
-
-# create wp instalation directory
+# crear el directorio
 mkdir -p /var/www/html
 
-# waits until the conection with mariadb through port 3306 is stablished (nc = netcat). Waits 1 sec between tries
+# Esperar hasta que el servicio mariadb esté en funcionamiento
 while ! nc -z mariadb 3306; do
     sleep 1
 done
 
 cd /var/www/html
 rm -rf *
-
-# Download wordpress
+# Descargar WP en español.
 wp core download --locale=es_ES --allow-root
-
-# create wp configuration file with specified database details (db name, user, password, host, language) 
+#Asegura que WordPress se conecte correctamente a la base de datos de MariaDB durante el proceso de instalación y configuración.
 wp --path=/var/www/html  config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --locale=es_ES --allow-root --skip-check
-
-# instal wordpress with provided parameters.
-# change site title. create admin user (with name, password, email). Skip email send.
+# Por ejemplo, puedes usar WP-CLI para configurar WordPress en este punto
+# Ejemplo: wp core install --url=http://mi-sitio.com --title="Mi Sitio" --admin_user=admin --admin_password=password --admin_email=info@mi-sitio.com
 wp core install --path=/var/www/html --url=${DOMAIN_NAME} --title="INZEZION!!!" --admin_name=${WP_ADMIN_USER} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email="imontero@student.42urduliz.com" --skip-email --allow-root 
-
-wp plugin install redis-cache --activate
-wp config set WP_REDIS_HOST redis --allow-root #I put --allowroot because i am on the root user on my VM
-wp config set WP_REDIS_PORT 6379 --raw --allow-root
-wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
-wp config set WP_REDIS_DATABASE "1"
-#wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
-wp config set WP_REDIS_CLIENT phpredis --allow-root
-wp plugin install redis-cache --activate --allow-root
-wp plugin update --all --allow-root
-wp redis enable --allow-root
